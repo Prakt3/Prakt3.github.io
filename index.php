24414ef842c849c0fda8ff099d3ff2b485e28af2
Prakt3 <Prakt3@yandex.ru>
2019-04-26 14:32:07 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>ИГра</title>
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="script.js"></script>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="pelena"></div>
+    <div id="game">
+    </div>
+<div class="black_modal"></div>
+</body>
+</html>
